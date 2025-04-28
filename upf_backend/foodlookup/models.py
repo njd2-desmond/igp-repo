6290ea4319b03c463fd8cbd10b4a5ec2c86a4a09
jordan-edg <jordanedgecombe36@gmail.com>
2025-04-28 21:34:19 +0100

    Connected PostgreSQL database to backend

--- upf_backend/foodlookup/models.py
+++ upf_backend/foodlookup/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Scan(models.Model):
+    scan_id = models.AutoField(primary_key=True)
+    scan_date = models.DateTimeField()
+    scan_product_name = models.CharField(max_length=255)
+    scan_nova_score = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'scan'
+        managed = False
+
+    def __str__(self):
+        return f"Scan {self.scan_ID}: {self.scan_product_name} by {self.user}"
--- upf_backend/users/models.py
+++ upf_backend/users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key = True)
+    user_dob = models.DateField()
+    user_email = models.EmailField(unique = True)
+    user_consent = models.BooleanField()
+    user_first_name = models.CharField(max_length=255)
+    user_last_name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'users'
+        managed = False
+    
+    def __str__(self):
+        return f"{self.user_first_name} {self.user_last_name}"
+
+class Survey(models.Model):
+    survey_id = models.AutoField(primary_key=True)
+    survey_q1 = models.CharField(max_length=255)
+    survey_q2 = models.CharField(max_length=255)
+    survey_q3 = models.BooleanField
+    survey_q4 = models.BooleanField
+    survey_q5 = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'survey'
+        managed = False
+
+    def __str__(self):
+        return f"Survey {self.survey_ID} for {self.user}"
