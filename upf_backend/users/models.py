bdb8ffce963242d2e70fb64e41c293f7412091cb
jordan-edg <jordanedgecombe36@gmail.com>
2025-04-29 16:17:12 +0100

    Minor changes to database structure, custom datatypes added to Survey table

--- upf_backend/users/models.py
+++ upf_backend/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
@@ -19,11 +20,42 @@ class User(models.Model):
 
 class Survey(models.Model):
     survey_id = models.AutoField(primary_key=True)
-    survey_q1 = models.CharField(max_length=255)
-    survey_q2 = models.CharField(max_length=255)
-    survey_q3 = models.BooleanField
-    survey_q4 = models.BooleanField
-    survey_q5 = models.CharField(max_length=255)
+    survey_q1 = models.CharField(
+        max_length = 50,
+        choices = [
+            ('Multiple times a day', 'Multiple times a day'),
+            ('Once a day', 'Once a day'),
+            ('A few times a week', 'A few times a week'),
+            ('Rarely', 'Rarely'),
+            ('Do Not Know', 'Do Not Know')
+        ],
+    )
+    survey_q2 = models.CharField(
+        max_length = 80,
+        choices = [
+            ('I struggle to identify UPFs in foods', 'I struggle to identify UPFs in foods'),
+            ('I don\'t have time to cook from scratch', 'I don\'t have time to cook from scratch'),
+            ('Healthy food is too expensive', 'Healthy food is too expensive'),
+            ('I crave processed food too much', 'I crave processed food too much'),
+            ('I don\'t know where to start', 'I don\'t know where to start')
+        ]
+    )
+    survey_q3 = models.BooleanField()
+    survey_q4 = models.BooleanField()
+    survey_q5 = models.CharField(
+        max_length = 80,
+        choices = [
+            ('Reduce my ultra-processed food intake', 'Reduce my ultra-processed food intake'),
+            ('Learn more about ultra-processed food', 'Learn more about ultra-processed food'),
+            ('Eat more whole, minimally processed foods', 'Eat more whole, minimally processed foods'),
+            ('Improve my overall nutrition and health', 'Improve my overall nutrition and health'),
+            ('Track how much ultra-processed food I consume', 'Track how much ultra-processed food I consume'),
+            ('Understand food labels and processing levels better', 'Understand food labels and processing levels better'),
+            ('Discover hidden ultra processed ingredients in my diet', 'Discover hidden ultra processed ingredients in my diet'),
+            ('Find healthier alternatives to my favourite processed foods', 'Find healthier alternatives to my favourite processed foods'),
+            ('Get personalised food recommendations based on my diet', 'Get personalised food recommendations based on my diet')
+        ]
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
@@ -31,4 +63,4 @@ class Survey(models.Model):
         managed = False
 
     def __str__(self):
-        return f"Survey {self.survey_ID} for {self.user}"
+        return f"Survey {self.survey_id} for {self.user}"
